2f6ab1e8847fec2267bf97b5888d0ab93c138e5a
DaNuwa <yhan7788@gmail.com>
2019-12-15 21:34:03 +0530

    Updates

---
+++ precache-manifest.a7efdef82754c6cc82c21fc9a28c53da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f09c7bc64c51950fb316977df2c80a1",
+    "url": "/Free-photo-stock-/index.html"
+  },
+  {
+    "revision": "abd731a211899e9f872a",
+    "url": "/Free-photo-stock-/static/css/main.303f0638.chunk.css"
+  },
+  {
+    "revision": "5da49db46d33d38f1688",
+    "url": "/Free-photo-stock-/static/js/2.afbf4acf.chunk.js"
+  },
+  {
+    "revision": "abd731a211899e9f872a",
+    "url": "/Free-photo-stock-/static/js/main.93cecfdd.chunk.js"
+  },
+  {
+    "revision": "b8ca7f759831531074c8",
+    "url": "/Free-photo-stock-/static/js/runtime-main.adfd8867.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Free-photo-stock-/precache-manifest.a7efdef82754c6cc82c21fc9a28c53da.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Free-photo-stock-/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
